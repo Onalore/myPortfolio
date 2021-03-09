8125cae358ad80ec0078c41d7c5e3051d951216d
ona <ona.lore5@gmail.com>
2021-03-09 13:56:52 -0300

    Proyect

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import { Grid, Container } from "@material-ui/core";
+
+export default function App() {
+  return <Grid container></Grid>;
+}
